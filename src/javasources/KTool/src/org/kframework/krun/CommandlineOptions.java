c42c8c5b314c8e9667fd9d775615b5030e8922e7
Owolabi Legunsen <owolabileg@gmail.com>
2014-01-27 13:50:12 -0600

    indexing: Added back option for indexing

--- src/javasources/KTool/src/org/kframework/krun/CommandlineOptions.java
+++ src/javasources/KTool/src/org/kframework/krun/CommandlineOptions.java
@@ -72,7 +72,7 @@ public class CommandlineOptions {
         addOptionS(OptionBuilder.withLongOpt("parser").hasArg().withArgName("command").withDescription("Command used to parse programs. (Default: kast).").create());
         addOptionS(OptionBuilder.withLongOpt("config-var-parser").hasArg().withArgName("file").withDescription("Command used to parse configuration variables. (Default: kast -e).  See --parser above. Applies to subsequent -c options until another parser is specified with this option.").create());
         addOptionS(OptionBuilder.withLongOpt("config-var").hasArgs(2).withValueSeparator('=').withArgName("name=value").withDescription("Specify values for variables in the configuration.").create("c"));
-        addOptionS(OptionBuilder.withLongOpt("output").hasArg().withArgName("mode").withDescription("How to display Maude results. <mode> is either [pretty|smart|kore|raw|binary|none]. (Default: pretty).").create("o"));
+        addOptionS(OptionBuilder.withLongOpt("output").hasArg().withArgName("mode").withDescription("How to display Maude results. <mode> is either [pretty|smart|raw|binary|none]. (Default: pretty).").create("o"));
 
         // Advanced K options
         addOptionS(OptionBuilder.withLongOpt("search").withDescription("In conjunction with it you can specify 3 options that are optional: pattern (the pattern used for search), bound (the number of desired solutions) and depth (the maximum depth of the search).").create());
@@ -87,7 +87,6 @@ public class CommandlineOptions {
         addOptionS(OptionBuilder.withLongOpt("backend").hasArg().withArgName("backend").withDescription("Specify the krun backend to execute with. <backend> is either [maude|java]. (Default: maude).").create());
         addOptionS(OptionBuilder.withLongOpt("simulation").hasArg().withArgName("string").withDescription("Simulation property of two programs in two semantics.").create());
         addOptionS(OptionBuilder.withLongOpt("help-experimental").withDescription("Print help on non-standard options.").create("X"));
-        addOptionS(OptionBuilder.withLongOpt("tempkoreoutput").withDescription("Output Kore file.").create());
 
 
         // Experimental options
@@ -106,6 +105,7 @@ public class CommandlineOptions {
         addOptionE(OptionBuilder.withLongOpt("prove").hasArg().withArgName("file").withDescription("Prove a set of reachability rules.").create());
         addOptionE(OptionBuilder.withLongOpt("smt").hasArg().withArgName("solver").withDescription("SMT solver to use for checking constraints. <solver> is either [z3|gappa|none]. (Default: z3).").create());
         addOptionE(OptionBuilder.withLongOpt("generate-tests").withDescription("Test programs will be generated along with normal search.").create());
+        addOptionE(OptionBuilder.withLongOpt("index").withDescription("Index the rules for faster rewriting.").create());
 
         addOptionE(OptionBuilder.withLongOpt("output-file").hasArg().withArgName("file").withDescription("Store output in the file instead of displaying it.").create());
         addOptionE(OptionBuilder.withLongOpt("load-cfg").hasArg().withArgName("file").withDescription("Load a configuration saved with the \"binary\" output mode into the $PGM configuration variable instead of a program.").create());
