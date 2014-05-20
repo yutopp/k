a0233f53c918093f9db29cbc9ec252dc00e58f26
qangel <liyili2@illinois.edu>
2014-05-20 13:02:52 -0500

    move flattenTerm before FreshCondToFreshVar

--- src/javasources/KTool/src/org/kframework/backend/BasicBackend.java
+++ src/javasources/KTool/src/org/kframework/backend/BasicBackend.java
@@ -75,13 +75,13 @@ public abstract class BasicBackend implements Backend {
         steps.add(new AddSymbolicK(context));
         steps.add(new AddSemanticEquality(context));
         // steps.add(new ResolveFresh());
+        steps.add(new FlattenTerms(context));
         steps.add(new FreshCondToFreshVar(context));
         steps.add(new ResolveFreshVarMOS(context));
         steps.add(new AddTopCellConfig(context));
         if (options.experimental.addTopCell) {
             steps.add(new AddTopCellRules(context));
         }
-        steps.add(new FlattenTerms(context));
         steps.add(new ResolveBinder(context));
         steps.add(new ResolveAnonymousVariables(context));
         steps.add(new AddK2SMTLib(context));
