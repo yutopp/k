f061a8ee4de08e5699e63c84c10049227990e1be
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2014-07-13 15:53:53 +0300

    Some updates.

--- src/javasources/KTool/src/org/kframework/backend/pdmc/k/JavaKRunPromelaEvaluator.java
+++ src/javasources/KTool/src/org/kframework/backend/pdmc/k/JavaKRunPromelaEvaluator.java
@@ -1,6 +1,9 @@
 package org.kframework.backend.pdmc.k;
 
-import org.kframework.backend.java.kil.Term;
+import com.google.common.collect.ImmutableList;
+import org.kframework.backend.java.builtins.BoolToken;
+import org.kframework.backend.java.builtins.StringToken;
+import org.kframework.backend.java.kil.*;
 import org.kframework.backend.pdmc.pda.*;
 import org.kframework.backend.pdmc.k.JavaKRunPushdownSystem;
 import org.kframework.backend.pdmc.pda.buchi.Evaluator;
@@ -12,9 +15,13 @@ import org.kframework.backend.pdmc.pda.buchi.Identifier;
 public class JavaKRunPromelaEvaluator implements Evaluator<ConfigurationHead<Term, Term>> {
 
     JavaKRunPushdownSystem pds;
+    TermContext termContext;
+    Definition definition;
 
     public JavaKRunPromelaEvaluator(JavaKRunPushdownSystem pds) {
         this.pds = pds;
+        termContext = pds.termContext;
+        definition = termContext.definition();
     }
 
     Term state;
@@ -26,7 +33,14 @@ public class JavaKRunPromelaEvaluator implements Evaluator<ConfigurationHead<Ter
 
     @Override
     public boolean evaluate(Identifier id) {
-        System.err.println(state.toString() + " |= " + id);
-        return false;
+        StringToken idString = StringToken.of(id.toString());
+        Term term = new KItem(KLabelConstant.of("'_|=_", definition),
+                new KList(ImmutableList.copyOf(new Term[]{KLabelInjection.injectionOf(state, termContext), idString})),
+                termContext);
+        System.out.print(term.toString());
+        Term result = term.evaluate(termContext);
+        System.out.println( " = " + result.toString());
+        assert result instanceof BoolToken;
+        return ((BoolToken) result).booleanValue();
     }
 }
