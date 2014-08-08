2c5d0106989a840ea0d1950531a22dde1e03f23b
qangel <liyili2@illinois.edu>
2014-08-08 13:51:52 -0500

    fixing style in Kast parser unit test

--- src/javasources/KTool/test/org/kframework/parser/kast/KastParserTest.java
+++ src/javasources/KTool/test/org/kframework/parser/kast/KastParserTest.java
@@ -7,12 +7,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+import org.kframework.kil.BoolBuiltin;
 import org.kframework.kil.KList;
 import org.kframework.kil.Bag;
 import org.kframework.kil.Cell;
 import org.kframework.kil.IntBuiltin;
 import org.kframework.kil.KApp;
 import org.kframework.kil.KLabelConstant;
+import org.kframework.kil.StringBuiltin;
 import org.kframework.kil.Term;
 import org.kframework.kil.loader.Context;
 
@@ -22,30 +24,36 @@ public class KastParserTest {
     public void test() {
         final Context context = new Context();
         //test input the .Bag
-        assertTrue(KastParser.parse("output.txt", ".Bag", context).getBody().equals(Bag.EMPTY));
+        assertEquals(KastParser.parse("output.txt", ".Bag", context).getBody(),Bag.EMPTY);
         //test input the number 0 and the number with ktoken format
-        assertTrue(KastParser.parse("output.txt", "0", context).getBody()
-                        .equals(KastParser.parse("output.txt", "`0::Int`", context).getBody()));
+        assertEquals(KastParser.parse("output.txt", "0", context).getBody(),IntBuiltin.of(0));
+        assertEquals(KastParser.parse("output.txt", "0", context).getBody(),
+                        KastParser.parse("output.txt", "`0::Int`", context).getBody());
         //test input the number 0 and the number with ktoken format without sort
-        assertTrue(KastParser.parse("output.txt", "0", context).getBody()
-                .equals(KastParser.parse("output.txt", "`0`", context).getBody()));
+        assertEquals(KastParser.parse("output.txt", "0", context).getBody(),
+                        KastParser.parse("output.txt", "`0`", context).getBody());
         //test input the string "0" and the number with ktoken format
-        assertTrue(KastParser.parse("output.txt", "`\"0\"`", context).getBody()
-                .equals(KastParser.parse("output.txt", "\"0\"", context).getBody()));
+        assertEquals(KastParser.parse("output.txt", "`\"0\"`", context).getBody(),
+                StringBuiltin.of("0"));
+        assertEquals(KastParser.parse("output.txt", "`\"0\"`", context).getBody(),
+                        KastParser.parse("output.txt", "\"0\"", context).getBody());
+        assertFalse(KastParser.parse("output.txt", "`\"\"0\"\"`", context).getBody()
+        		.equals(KastParser.parse("output.txt", "\"0\"", context).getBody()));
         //test input boolean format
-        assertTrue(KastParser.parse("output.txt", "true", context).getBody()
-                .equals(KastParser.parse("output.txt", "`true::Bool`", context).getBody()));
-        assertTrue(KastParser.parse("output.txt", "false", context).getBody()
-                .equals(KastParser.parse("output.txt", "`false::Bool`", context).getBody()));
+        assertEquals(KastParser.parse("output.txt", "true", context).getBody(),
+                 BoolBuiltin.of("true"));
+        assertEquals(KastParser.parse("output.txt", "true", context).getBody(),
+                 KastParser.parse("output.txt", "`true::Bool`", context).getBody());
+        assertEquals(KastParser.parse("output.txt", "false", context).getBody(),
+                BoolBuiltin.of("false"));
+        assertEquals(KastParser.parse("output.txt", "false", context).getBody(),
+                KastParser.parse("output.txt", "`false::Bool`", context).getBody());
         //test parsing klabel and cells
         Cell testCell = new Cell("abc",Bag.EMPTY);
-        assertTrue(KastParser.parse("output.txt", "<abc> .Bag </abc>", context)
-                    .getBody().equals(testCell));
+        assertEquals(KastParser.parse("output.txt", "<abc> .Bag </abc>", context),
+        		testCell);
         //test parsing a KItem
-        List<Term> testKList = new ArrayList<Term>();
-        testKList.add(IntBuiltin.of(0));
-        testKList.add(IntBuiltin.of(0));
-        assertTrue(KastParser.parse("output.txt", "`abc`(0,0)", context)
-                .getBody().equals(new KApp(KLabelConstant.of("abc"), (Term)(new KList(testKList)))));
+        assertEquals(KastParser.parse("output.txt", "`abc`(0,0)", context).getBody(),
+        		KApp.of("abc", IntBuiltin.of(0), IntBuiltin.of(0)));
     }
 }
