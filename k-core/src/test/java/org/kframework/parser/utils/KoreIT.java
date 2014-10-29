d4ddcd66d750d28b189a4833faa9770116126ad6
Radu Mereuta <headness13@gmail.com>
2014-10-29 11:41:48 -0500

    Fixing the test. Adding IT at the end to be built in the Integration Test part.

--- k-core/src/test/java/org/kframework/parser/utils/KoreIT.java
+++ k-core/src/test/java/org/kframework/parser/utils/KoreIT.java
@@ -2,7 +2,6 @@
 package org.kframework.parser.utils;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.Assert;
 import org.junit.Test;
 import org.kframework.kil.Definition;
 import org.kframework.kil.ProductionReference;
@@ -15,15 +14,19 @@ import org.kframework.utils.file.JarInfo;
 
 import java.io.File;
 
-public class KoreTest extends BaseTestCase {
+/**
+ * Test a the kore definition using the new parser.
+ * KoreIT has to be run after the distribution has been built (apparently), therefore the name IT at the end.
+ */
+public class KoreIT extends BaseTestCase {
 
     @Test
     public void testKore() throws Exception {
-        System.out.println(JarInfo.getKBase(false));
+        //System.out.println(JarInfo.getKBase(false));
         String quq = FileUtils.readFileToString(new File(JarInfo.getKBase(false) + "/samples/kast/quote-unquote.kore"));
         String kore = FileUtils.readFileToString(new File(JarInfo.getKBase(false) + "/samples/kast/kore.k"));
         Definition def = new Definition();
-        def.setItems(Outer.parse(Sources.generatedBy(KoreTest.class), kore, null));
+        def.setItems(Outer.parse(Sources.generatedBy(KoreIT.class), kore, null));
         ProductionReference pr = null;
         try {
             pr = QuickParser.parse(quq, Sort.of("KDefinition"), def, kem);
@@ -31,7 +34,7 @@ public class KoreTest extends BaseTestCase {
             System.err.println(e.getMessage() + " Line: " + e.getKException().getLocation().lineStart + " Column: " + e.getKException().getLocation().columnStart);
             assert false;
         }
-        System.out.println(pr);
+        //System.out.println(pr);
 
         //Assert.assertEquals("Expected Nullable NTs", true, nc.isNullable(nt1.entryState) && nc.isNullable(nt1.exitState));
         //Assert.assertEquals("Expected Nullable NTs", true, nc.isNullable(nt1));
